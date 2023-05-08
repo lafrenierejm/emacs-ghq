a5de31e0e37a4a61bf5f425cece2a1e209498977
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-05-08 11:49:48 -0500

    Allow user to run arbitrary functions on cloned path
    Wrapping the execution of `ghq get` and `ghq list` in `set-process-sentinel'
    allows waiting for process completion without blocking.

--- ghq.el
+++ ghq.el
@@ -6,7 +6,7 @@
 ;; Author: Roman Coedo <romancoedo@gmail.com>
 ;; Created 28 November 2015
 ;; Version: 0.1.3
-;; Package-Requires: ()
+;; Package-Requires: ((emacs "26.1") (dash "2.18.0") (s "1.7.0"))
 
 ;; Keywords: ghq
 
@@ -35,6 +35,9 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+(require 'dash)
+(require 'rx)
+(require 's)
 (require 'simple)
 
 (defun ghq--find-root ()
@@ -44,6 +47,9 @@
 (defvar ghq--root
   (ghq--find-root))
 
+(defcustom ghq-after-clone-functions nil
+  "List of functions to be called on the path of a newly cloned repository.")
+
 (defun ghq--find-projects ()
   "Find the list of ghq projects relative to ghq root."
   (split-string (shell-command-to-string "ghq list")))
@@ -52,13 +58,41 @@
   "Find the list of ghq projects."
   (split-string (shell-command-to-string "ghq list --full-path")))
 
-(defun ghq--get-repository (repository)
-  "Get the REPOSITORY."
-  (async-shell-command (concat "ghq get " repository)))
+;;;###autoload
+(defun ghq (repository &optional ssh)
+  "Clone REPOSITORY via ghq, optionally over SSH."
+  (interactive "MEnter the repository: \nP")
+  (let* ((clone-command (-non-nil `("ghq" "get" ,(when ssh "-p") ,repository)))
+         (clone-command-string (s-join " " clone-command)))
+    (set-process-sentinel
+     (apply #'start-file-process clone-command-string nil clone-command)
+     `(lambda (process event)
+        (when (eq (process-status process) 'exit)
+          (let* ((list-command (list "ghq" "list" "--full-path" "--exact" ,repository))
+                 (list-command-string (s-join " " list-command))
+                 (buffer (generate-new-buffer (s-wrap list-command-string "*"))))
+            (set-process-sentinel
+             (apply #'start-file-process list-command-string buffer list-command)
+             `(lambda (process event)
+                (when (memq (process-status process) '(exit signal))
+                  (when-let
+                      ((path (with-current-buffer ,buffer
+                               (goto-char (point-min))
+                               (re-search-forward
+                                (rx
+                                 (seq line-start (group-n 1 (one-or-more not-newline)) line-end)))
+                               (match-string 1))))
+                    (message "%s cloned to %s" ,,repository path)
+                    (when path
+                      (dolist (fun ghq-after-clone-functions)
+                        (funcall fun path)))))))))))))
 
-(defun ghq--get-repository-ssh (repository)
-  "Get the REPOSITORY using ssh."
-  (async-shell-command (concat "ghq get -p " repository)))
+;;;###autoload
+(defun ghq-ssh ()
+  "Clone a repository via ghq over SSH."
+  (interactive)
+  (let ((current-prefix-arg t))
+    (call-interactively #'ghq)))
 
 (defvar ghq--helm-action
   '(("Open Dired"              . (lambda (dir) (dired              (concat ghq--root "/" dir))))
@@ -77,17 +111,6 @@
   (helm-make-source "Search ghq projects with helm" 'helm-source-sync
     :candidates (ghq--find-projects)
     :action ghq--helm-action))
-
-;;;###autoload
-(defun ghq ()
-  "Get the repository via ghq."
-  (interactive)
-  (ghq--get-repository (read-string "Enter the repository: ")))
-
-(defun ghq-ssh ()
-  "Get the repository via ghq using ssh."
-  (interactive)
-  (ghq--get-repository-ssh (read-string "Enter the repository: ")))
 
 (defun ghq-list ()
   "Display the ghq project list in a message."
